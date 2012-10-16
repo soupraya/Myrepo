eabda47af1a3a42a20ccc1df155e161d95ee7ed4
Souprayane <Souprayane@Souprayane-PC>
2012-10-16 22:09:06 +0200

    apache http client replace by gwt http client

--- src/com/soupraya/server/GreetingServiceImpl.java
+++ src/com/soupraya/server/GreetingServiceImpl.java
@@ -3,6 +3,11 @@ package com.soupraya.server;
 import com.soupraya.client.GreetingService;
 import com.soupraya.shared.FieldVerifier;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import com.google.gwt.http.client.*;
+import com.google.gwt.http.client.URL;
 
 /**
  * The server side implementation of the RPC service.
@@ -45,4 +50,78 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements
 		return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;")
 				.replaceAll(">", "&gt;");
 	}
+	
+
+	
+	    private String ConnectZoho()
+	    {    
+	           String authtoken = "118e4371851e32ecbbf4f7a4ef0211d8";
+	           String targetURL = "https://crm.zoho.com/crm/private/xml/Leads/getRecords"; 
+	           String paramname = "content";
+	            
+	           RequestBuilder builder = new RequestBuilder(RequestBuilder.GET, URL.encode(targetURL));
+	           try {
+	        	   Request request = builder.sendRequest(null, new RequestCallback() {
+	        	     public void onError(Request request, Throwable exception) {
+	        	        // Couldn't connect to server (could be timeout, SOP violation, etc.)     
+	        	     }
+
+	        	     public void onResponseReceived(Request request, Response response) {
+	        	       if (200 == response.getStatusCode()) {
+	        	           // Process the response in response.getText()
+	        	       } else {
+	        	         // Handle the error.  Can get the status text from response.getStatusText()
+	        	       }
+	        	     }       
+	        	   });
+	        	 } catch (RequestException e) {
+	        	   // Couldn't connect to server        
+	        	 }
+	           
+			return paramname;
+	            
+	            
+/*	            PostMethod post = new PostMethod(targetURL);
+	            post.setParameter("authtoken",authtoken);
+	            post.setParameter("scope","crmapi");
+	            HttpClient httpclient = new HttpClient();
+	            PrintWriter myout = null;
+
+	            -------------------------------------- Execute the http request--------------------------------
+	            try 
+	            {
+	                long t1 = System.currentTimeMillis();
+	                int result = httpclient.executeMethod(post);
+	                System.out.println("HTTP Response status code: " + result);
+	                System.out.println(">> Time taken " + (System.currentTimeMillis() - t1));
+	            -------------------------------------- Execute the http request--------------------------------
+	                
+	                 ---------------------------writing the response to a file--------------------
+	                myout = new PrintWriter(new File("response.xml"));
+	                myout.print(post.getResponseBodyAsString());
+	                ---------------------------writing the response to a file--------------------
+	                
+	                -----------------------Get response as a string ----------------
+	                String postResp = post.getResponseBodyAsString();
+	                System.out.println("postResp=======>"+postResp);
+	                 ---------------------Get response as a string ----------------------------
+	                 if(postResp.equals("Invalid Ticket Id"))
+	                 {
+	                     // generate new auth token and call the API
+	                 } 
+	            }
+	            catch(Exception e)
+	            {
+	                e.printStackTrace();
+	            }    
+	            finally 
+	            {
+	                myout.close();
+	                post.releaseConnection();
+	            }
+	        }
+
+	    }*/ 
+  
+}
 }
